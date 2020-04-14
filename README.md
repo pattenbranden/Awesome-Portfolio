3cdf4ec22b770a857ff18cd0d26172f1f633a2db
pattenbranden <patten.branden@gmail.com>
2020-04-14 17:57:52 -0400

    adding initial server files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Awesome-Portfolio
+
+This is my current portfolio page.
+In this project I've used Express, Bootstrap, and Handlebars primarily. 
+
+The bubbles used for the background were one of the hardest parts to get together for this page as it was my first time doing something like this. I learned about "webkit", "moz", and "o" keyframes, very interesting seeing how different browsers render things slightly differently. I also learned about the css property "overflow", since the bubbles shifting to off the side of the page created a scroll bar as long as they were there. 
+
+I also learned about incoroporating custom fonts on the server as "woff" files, and how to apply them.
+
+Included are links to my linked-in and github as well as a link to serve my resume as a PDF.
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,35 @@
+var db = require("../models");
+var path = require("path")
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+      res.render("about");
+  });
+  app.get("/about", function(req, res) {
+      res.render("about");
+  });
+  app.get("/contact", function(req, res) {
+      res.render("contact");
+  });
+  app.get("/projects", function(req, res) {
+      res.render("projects");
+  });
+  app.get("/resume", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/resume/Branden-Patten.pdf"));
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
