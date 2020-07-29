ef1537e48befc0f2010d53b3bf499d595cb13dc1
Branden Patten <patten.branden@gmail.com>
2020-07-29 04:18:39 -0400

    Removed sequalize, not used in this project

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
 
-var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -33,15 +31,13 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+// Starting the server
+app.listen(PORT, function() {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
 });
 
 module.exports = app;
